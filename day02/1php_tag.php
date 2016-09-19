41562f24e65f206a91a3b2597007bd752c27f4b8
windbamboo <windbamboo@163.com>
2016-09-19 17:56:57 +0800

    init

---
+++ day02/1php_tag.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuguozhu
+ * Date: 2016/9/19
+ * Time: 17:26
+ */
+//推荐
+echo "<hr/>";
+echo "<p>这是一个段落</p>";
+?>
+    111111
+<?
+//不推荐
+echo "<hr />";
+?>
+
+    22222
+<?php
+echo "<h1>这个是标题...</h1>";
+
+
+
