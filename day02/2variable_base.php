30dfc351d703bb20c39274c6871ec143d13e502a
windbamboo <windbamboo@163.com>
2016-09-20 23:21:38 +0800

    initinit

--- day02/2variable_base.php
+++ day02/2variable_base.php
@@ -5,3 +5,15 @@
  * Date: 2016/9/19
  * Time: 18:01
  */
+
+$v1 = 1;
+$v2 = null;
+if (isset($v1)) {
+    echo "<br/>v1存在";
+}
+if (isset($v2)){
+    echo "<br/>v2存在";
+}
+if (isset($v3)){
+    echo "<br/>v3存在";
+}
