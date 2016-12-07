ada3af4f7d89fecc627bcd188ef0e812401b4cd9
sundy <1159279222@qq.com>
2016-12-07 17:00:09 +0800

    init day04

--- day04/2zuoye3.php
+++ day04/2zuoye3.php
@@ -1,7 +1,50 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh-cn">
+<head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+    <title>网页标题</title>
+    <meta name="keywords" content="关键字列表"/>
+    <meta name="description" content="网页描述"/>
+    <link rel="stylesheet" type="text/css" href=""/>
+    <style type="text/css"></style>
+    <script type="text/javascript"></script>
+</head>
+<body>
+
 <?php
-/**
- * Created by PhpStorm.
- * User: liuguozhu
- * Date: 2016/12/5
- * Time: 19:48
- */
+$n1 = $n2 = "";
+if ($_POST) {
+    $n1 = $_POST['n1'];
+    $trans = $_POST['trans'];
+    if ($trans == 1) {
+        $n2 = decbin($n1);
+    } else if ($trans == 2) {
+        $n2 = decoct($n1);
+    } else if ($trans == 3) {
+        $n2 = dechex($n1);
+    } else if ($trans == 4) {
+        $n2 = bindec($n1);
+    } else if ($trans == 5) {
+        $n2 = octdec($n1);
+    } else if ($trans == 6) {
+        $n2 = hexdec($n1);
+    }
+}
+?>
+
+<form action="" method="post">
+    <input type="text" name="n1" value="<?php echo $n1 ?>"/>
+    <select name="trans">
+        <option value="1">10to2</option>
+        <option value="2">10to8</option>
+        <option value="3">10to16</option>
+        <option value="4">2to10</option>
+        <option value="5">8to10</option>
+        <option value="6">16to10</option>
+    </select>
+    <input type="submit" name="submit1" value="转换"/>
+    <input type="text" name="n2" value="<?php echo $n2;?>"/>
+</form>
+</body>
+</html>
